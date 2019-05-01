317248853ce3ec93032c5c06ca6b8a95d811aefd
veritas9872 <veritas9872@gmail.com>
2019-05-01 12:21:44 +0900

    Copied even more code from Facebook and from my previous projects.

--- data/data_transforms.py
+++ data/data_transforms.py
@@ -231,3 +231,124 @@ def tensor_to_complex_np(data):
     """
     data = data.numpy()
     return data[..., 0] + 1j * data[..., 1]
+
+
+class DataTrainTransform:
+    """
+    Data Transformer for training U-Net models.
+    """
+
+    def __init__(self, mask_func, resolution, which_challenge, use_seed=True):
+        """
+        Args:
+            mask_func (common.subsample.MaskFunc): A function that can create a mask of
+                appropriate shape.
+            resolution (int): Resolution of the image.
+            which_challenge (str): Either "singlecoil" or "multicoil" denoting the dataset.
+            use_seed (bool): If true, this class computes a pseudo random number generator seed
+                from the filename. This ensures that the same mask is used for all the slices of
+                a given volume every time.
+        """
+        if which_challenge not in ('singlecoil', 'multicoil'):
+            raise ValueError(f'Challenge should either be "singlecoil" or "multicoil"')
+        self.mask_func = mask_func
+        self.resolution = resolution
+        self.which_challenge = which_challenge
+        self.use_seed = use_seed
+
+    def __call__(self, kspace, target, attrs, file_name, slice_num):
+        """
+        Args:
+            kspace (numpy.array): Input k-space of shape (num_coils, rows, cols, 2) for multi-coil
+                data or (rows, cols, 2) for single coil data.
+            target (numpy.array): Target image
+            attrs (dict): Acquisition related information stored in the HDF5 object.
+            file_name (str): File name
+            slice_num (int): Serial number of the slice.
+        Returns:
+            (tuple): tuple containing:
+                image (torch.Tensor): Zero-filled input image.
+                target (torch.Tensor): Target image converted to a torch Tensor.
+                mean (float): Mean value used for normalization.
+                std (float): Standard deviation value used for normalization.
+                norm (float): L2 norm of the entire volume.
+        """
+        kspace = to_tensor(kspace)
+        # Apply mask
+        seed = None if not self.use_seed else tuple(map(ord, file_name))
+        masked_kspace, mask = apply_mask(kspace, self.mask_func, seed)
+        # Inverse Fourier Transform to get zero filled solution
+        image = ifft2(masked_kspace)
+        # Crop input image
+        image = complex_center_crop(image, (self.resolution, self.resolution))
+        # Absolute value
+        image = complex_abs(image)
+        # Apply Root-Sum-of-Squares if multicoil data
+        if self.which_challenge == 'multicoil':
+            image = root_sum_of_squares(image)
+        # Normalize input
+        image, mean, std = normalize_instance(image, eps=1e-11)
+        image = image.clamp(-6, 6)
+
+        target = to_tensor(target)
+        # Normalize target
+        target = normalize(target, mean, std, eps=1e-11)
+        target = target.clamp(-6, 6)
+        return image, target, mean, std, attrs['norm'].astype(np.float32)  # TODO: Change this!
+
+
+class DataValTransform:
+    """
+    Data Transformer for running U-Net models on a validation dataset.
+    """
+
+    def __init__(self, resolution, which_challenge, mask_func=None):
+        """
+        Args:
+            resolution (int): Resolution of the image.
+            which_challenge (str): Either "singlecoil" or "multicoil" denoting the dataset.
+            mask_func (common.subsample.MaskFunc): A function that can create a mask of
+                appropriate shape.
+        """
+        if which_challenge not in ('singlecoil', 'multicoil'):
+            raise ValueError(f'Challenge should either be "singlecoil" or "multicoil"')
+        self.resolution = resolution
+        self.which_challenge = which_challenge
+        self.mask_func = mask_func
+
+    def __call__(self, kspace, target, attrs, file_name, slice_num):
+        """
+        Args:
+            kspace (numpy.Array): k-space measurements
+            target (numpy.Array): Target image
+            attrs (dict): Acquisition related information stored in the HDF5 object
+            file_name (pathlib.Path): Path to the input file
+            slice_num (int): Serial number of the slice
+        Returns:
+            (tuple): tuple containing:
+                image (torch.Tensor): Normalized zero-filled input image
+                mean (float): Mean of the zero-filled image
+                std (float): Standard deviation of the zero-filled image
+                file_name (pathlib.Path): Path to the input file
+                slice_num (int): Serial number of the slice
+        """
+        kspace = to_tensor(kspace)
+        if self.mask_func is not None:
+            seed = tuple(map(ord, file_name))  # TODO: Check if this is a bug or not.
+            masked_kspace, _ = apply_mask(kspace, self.mask_func, seed)
+        else:
+            masked_kspace = kspace
+        # Inverse Fourier Transform to get zero filled solution
+        image = ifft2(masked_kspace)
+        # Crop input image
+        image = complex_center_crop(image, (self.resolution, self.resolution))
+        # Absolute value
+        image = complex_abs(image)
+        # Apply Root-Sum-of-Squares if multicoil data
+        if self.which_challenge == 'multicoil':
+            image = root_sum_of_squares(image)
+        # Normalize input
+        image, mean, std = normalize_instance(image)
+        image = image.clamp(-6, 6)
+        return image, mean, std, file_name, slice_num
+
---
+++ utils/train_utils.py
@@ -0,0 +1,75 @@
+import torch
+from torch import nn, optim
+from pathlib import Path
+
+
+class CheckpointManager:
+    """
+    A checkpoint manager for Pytorch models and optimizers loosely based on Keras/Tensorflow Checkpointers.
+    I should note that I am not sure whether this works in Pytorch graph mode.
+    Giving up on saving as HDF5 files like in Keras. Just too annoying.
+    Note that the whole system is based on 1 indexing, not 0 indexing.
+    """
+    def __init__(self, model, optimizer, save_best_only=True, ckpt_dir='./checkpoints', max_to_keep=5):
+
+        # Type checking.
+        assert isinstance(model, nn.Module), 'Not a Pytorch Model'
+        assert isinstance(optimizer, optim.Optimizer), 'Not a Pytorch Optimizer'
+        assert isinstance(max_to_keep, int) and (max_to_keep >= 0), 'Not a non-negative integer'
+        ckpt_path = Path(ckpt_dir)
+        assert ckpt_path.exists(), 'Not a valid, existing path'
+
+        record_path = ckpt_path / 'Checkpoints.txt'
+
+        try:
+            record_file = open(record_path, mode='x')
+        except FileExistsError:
+            import sys
+            print('WARNING: It is recommended to have a separate checkpoint directory for each run.', file=sys.stderr)
+            print('Appending to previous Checkpoint record file!', file=sys.stderr)
+            record_file = open(record_path, mode='a')
+
+        print(f'Checkpoint List for {ckpt_path}', file=record_file)
+        record_file.close()
+
+        self.model = model
+        self.optimizer = optimizer
+        self.save_best_only = save_best_only
+        self.ckpt_path = ckpt_path
+        self.max_to_keep = max_to_keep
+        self.save_counter = 0
+        self.record_path = record_path
+        self.record_dict = dict()
+
+    def _save(self, ckpt_name=None):
+        self.save_counter += 1
+        save_dict = {'model_state_dict': self.model.state_dict(), 'optimizer_state_dict': self.optimizer.state_dict()}
+        save_path = self.ckpt_path / (f'{ckpt_name}.tar' if ckpt_name else f'ckpt_{self.save_counter:03d}.tar')
+
+        torch.save(save_dict, save_path)
+        print(f'Saved Checkpoint to {save_path}')
+        print(f'Checkpoint {self.save_counter:04d}: {save_path}')
+        # Save to file.
+        self.record_path.write_text(f'Checkpoint {self.save_counter:04d}: {save_path}\n')
+
+        self.record_dict[self.save_counter] = save_path
+
+        if self.save_counter > self.max_to_keep:
+            for count, ckpt_path in self.record_dict.items():  # This system uses 1 indexing.
+                if (count <= (self.save_counter - self.max_to_keep)) and ckpt_path.exists():
+                    ckpt_path.unlink()  # Delete existing checkpoint
+
+    def save(self, is_best=False, ckpt_name=None):
+        if is_best or not self.save_best_only:
+            self._save(ckpt_name)
+
+    def load(self, load_dir, load_optimizer=True):
+        save_dict = torch.load(load_dir)
+
+        self.model.load_state_dict(save_dict['model_state_dict'])
+
+        if load_optimizer:
+            self.optimizer.load_state_dict(save_dict['optimizer_state_dict'])
+
+    def load_latest(self, load_root):
+        pass
