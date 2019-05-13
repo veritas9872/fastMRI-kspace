Fixed image display issue. However, I found a new issue with numerical underflow.

train/training.py
@@ -1,6 +1,4 @@
 import torch
-import torchvision
-import torchsummary
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
@@ -17,7 +15,7 @@ from utils.run_utils import get_logger, initialize, save_dict_as_json
 from data.mri_data import SliceData
 from data.slice_transforms import TrainSliceTransform
 
-from models.k_unet_model import UnetModel
+from models.k_unet_model import UnetModel  # TODO: Create method to specify model in main.py
 
 """
 Please note a bit of terminology. 
@@ -116,8 +114,8 @@ def train_epoch(model, optimizer, loss_func, data_loader, device, epoch, verbose
                     for step_metric in step_metrics:
                         print(f'Training metric Epoch {epoch:03d} Step {idx:03d}: {step_metric.item():.4e}')
 
-    epoch_loss = np.nanmean(epoch_loss_lst)  # Remove nan values just in case.
-    epoch_metrics = [np.nanmean(epoch_metric_lst) for epoch_metric_lst in epoch_metrics_lst] if metrics else None
+    epoch_loss = float(np.nanmean(epoch_loss_lst))  # Remove nan values just in case.
+    epoch_metrics = [float(np.nanmean(epoch_metric_lst)) for epoch_metric_lst in epoch_metrics_lst] if metrics else None
 
     num_nans = np.isnan(epoch_loss_lst).sum()
     if num_nans > 0:
@@ -126,36 +124,41 @@ def train_epoch(model, optimizer, loss_func, data_loader, device, epoch, verbose
     return epoch_loss, epoch_metrics
 
 
-def val_step(model, loss_func, inputs, targets):
-    recons = model(inputs, targets.shape)
-    step_loss = loss_func(recons, targets)
-    return step_loss, recons
-
-
 def make_grid_triplet(recons, targets):
     assert recons.shape == targets.shape
     if recons.size(0) > 1:
         raise NotImplementedError('Mini-batch size greater than 1 has not been implemented yet.')
 
-    recons = recons.detach().cpu()
-    targets = targets.detach().cpu()
+    # Assumes batch size of 1.
+    recons = recons.detach().cpu().squeeze(dim=0)
+    targets = targets.detach().cpu().squeeze(dim=0)
 
     large = torch.max(targets)
     small = torch.min(targets)
     diff = large - small
 
-    view_recons = (recons.clamp(min=small, max=large) - small) / diff
-    view_targets = (targets - small) / diff
+    # Scaling to 0~1 range.
+    recons = (recons.clamp(min=small, max=large) - small) / diff
+    targets = (targets - small) / diff
 
-    view_recons = torch.squeeze(view_recons, dim=0).unsqueeze(dim=1)
-    view_targets = torch.squeeze(view_targets, dim=0).unsqueeze(dim=1)
+    if recons.size(0) == 15:
+        recons = torch.cat(torch.chunk(recons.view(size=(-1, recons.size(-1))), chunks=5, dim=0), dim=1)
+        targets = torch.cat(torch.chunk(targets.view(size=(-1, targets.size(-1))), chunks=5, dim=0), dim=1)
+    elif recons.size(0) == 1:
+        recons = recons.squeeze()
+        targets = targets.squeeze()
+    else:
+        raise ValueError('Invalid dimensions!')
 
-    recons_grid = torchvision.utils.make_grid(view_recons)
-    targets_grid = torchvision.utils.make_grid(view_targets)
+    deltas = targets - recons
 
-    deltas_grid = targets_grid - recons_grid
+    return recons, targets, deltas
 
-    return recons_grid, targets_grid, deltas_grid
+
+def val_step(model, loss_func, inputs, targets):
+    recons = model(inputs, targets.shape)
+    step_loss = loss_func(recons, targets)
+    return step_loss, recons
 
 
 def val_epoch(model, loss_func, data_loader, writer, device, epoch, max_imgs=0, verbose=True, metrics=None):
@@ -175,27 +178,25 @@ def val_epoch(model, loss_func, data_loader, writer, device, epoch, max_imgs=0, 
             if metrics:
                 step_metrics = [metric(recons, targets) for metric in metrics]
                 for step_metric, epoch_metric_lst in zip(step_metrics, epoch_metrics_lst):
-                    epoch_metric_lst.append(step_metric.item())  # Equation for running average.
+                    epoch_metric_lst.append(step_metric.item())
 
             if verbose:
-                print(f'Validation loss Epoch {epoch:03d} Step {step:03d}: {step_loss.item():.4e}')
+                print(f'Epoch {epoch:03d} Step {step:03d} Validation loss: {step_loss.item():.4e}')
                 if metrics:
-                    for step_metric in step_metrics:
-                        print(f'Validation metric Epoch {epoch:03d} Step {step:03d}: {step_metric.item():.4e}')
+                    for idx, step_metric in enumerate(step_metrics):
+                        print(f'Epoch {epoch:03d} Step {step:03d}: Validation metric {idx}: {step_metric.item():.4e}')
 
             if max_imgs:
                 interval = len(data_loader.dataset) // max_imgs
                 if step % interval == 0:  # Note that all images are scaled independently of all other images.
-
                     assert isinstance(writer, SummaryWriter)
-
                     recons_grid, targets_grid, deltas_grid = make_grid_triplet(recons, targets)
-                    writer.add_image('Recons', recons_grid, epoch)
-                    writer.add_image('Targets', targets_grid, epoch)
-                    writer.add_image('Deltas', deltas_grid, epoch)
+                    writer.add_image(f'Recons', recons_grid, epoch, dataformats='HW')
+                    writer.add_image(f'Targets', targets_grid, epoch, dataformats='HW')
+                    writer.add_image(f'Deltas', deltas_grid, epoch, dataformats='HW')
 
-    epoch_loss = np.nanmean(epoch_loss_lst)  # Remove nan values just in case.
-    epoch_metrics = [np.nanmean(epoch_metric_lst) for epoch_metric_lst in epoch_metrics_lst] if metrics else None
+    epoch_loss = float(np.nanmean(epoch_loss_lst))  # Remove nan values just in case.
+    epoch_metrics = [float(np.nanmean(epoch_metric_lst)) for epoch_metric_lst in epoch_metrics_lst] if metrics else None
 
     num_nans = np.isnan(epoch_loss_lst).sum()
     if num_nans > 0:
@@ -205,6 +206,9 @@ def val_epoch(model, loss_func, data_loader, writer, device, epoch, max_imgs=0, 
 
 
 def train_model(args):
+    # TODO: I found that the loss disappears due to numerical underflow when trained like this.
+    #  I need to implement multiplication to make training more stable.
+    #  I have verified that training is possible.
 
     if args.batch_size > 1:
         raise NotImplementedError('Only batch size of 1 for now.')
@@ -241,11 +245,11 @@ def train_model(args):
     data_chans = 2 if args.challenge == 'singlecoil' else 30  # Multicoil has 15 coils with 2 for real/imag
     # data_chans indicates the number of channels in the data.
     model = UnetModel(in_chans=data_chans, out_chans=data_chans, chans=args.chans,
-                      num_pool_layers=args.num_pool_layers).to(device)
+                      num_pool_layers=args.num_pool_layers).to(device)  # TODO: Move to main.py
 
     optimizer = optim.Adam(model.parameters(), lr=args.init_lr)
-    loss_func = nn.MSELoss(reduction='mean').to(device)
-    metrics = None
+    loss_func = nn.L1Loss(reduction='mean').to(device)  # TODO: move to main.py
+    metrics = None  # TODO: Move to main.py. I wish to specify metrics and loss on main.py.
 
     checkpointer = CheckpointManager(model=model, optimizer=optimizer, mode='min', save_best_only=args.save_best_only,
                                      ckpt_dir=ckpt_path, max_to_keep=args.max_to_keep)
@@ -253,12 +257,12 @@ def train_model(args):
     if hasattr(args, 'previous_model') and args.previous_model:
         checkpointer.load(load_dir=checkpointer, load_optimizer=False)
 
-    scheduler = optim.lr_scheduler.ReduceLROnPlateau(
+    scheduler = optim.lr_scheduler.ReduceLROnPlateau(  # TODO: This should also be moved to main.py
         optimizer, mode='min', factor=0.1, patience=5, verbose=True, cooldown=0, min_lr=1E-7)
 
     writer = SummaryWriter(log_dir=str(log_path))
 
-    # example_inputs = torch.ones(size=(1, 2, 640, 328)).to(device, non_blocking=True)
+    # example_inputs = torch.ones(size=(1, 30, 640, 328)).to(device, non_blocking=True)
     # writer.add_graph(model=model, input_to_model=example_inputs)
 
     logger.info('Beginning Training loop')

utils/train_utils.py
@@ -51,10 +51,10 @@ class CheckpointManager:
         else:
             raise TypeError('Mode must be either `min` or `max`')
 
-    def _save(self, ckpt_name=None, **save_args):
+    def _save(self, ckpt_name=None, **save_kwargs):
         self.save_counter += 1
         save_dict = {'model_state_dict': self.model.state_dict(), 'optimizer_state_dict': self.optimizer.state_dict()}
-        save_dict.update(save_args)
+        save_dict.update(save_kwargs)
         save_path = self.ckpt_path / (f'{ckpt_name}.tar' if ckpt_name else f'ckpt_{self.save_counter:03d}.tar')
 
         torch.save(save_dict, save_path)
@@ -73,7 +73,7 @@ class CheckpointManager:
 
         return save_path
 
-    def save(self, metric, verbose=True, ckpt_name=None, **save_args):  # save_args are extra variables to save
+    def save(self, metric, verbose=True, ckpt_name=None, **save_kwargs):  # save_kwargs are extra variables to save
         if self.mode == 'min':
             is_best = metric < self.prev_best
         elif self.mode == 'max':
@@ -83,7 +83,7 @@ class CheckpointManager:
 
         save_path = None
         if is_best or not self.save_best_only:
-            save_path = self._save(ckpt_name)
+            save_path = self._save(ckpt_name, **save_kwargs)
 
         if verbose:
             if is_best:
@@ -112,3 +112,15 @@ class CheckpointManager:
 
     def load_latest(self, load_root):
         pass
+
+
+def load_model_from_checkpoint(model, load_dir):
+    """
+    A simple function for loading checkpoints without having to use Checkpoint Manager. Very useful for evaluation.
+    Checkpoint manager was designed for loading checkpoints before resuming training.
+    """
+    assert isinstance(model, nn.Module), 'Model must be a Pytorch module.'
+    assert Path(load_dir).exists(), 'The specified directory does not exist'
+    save_dict = torch.load(load_dir)
+    model.load_state_dict(save_dict['model_state_dict'])
+    return model