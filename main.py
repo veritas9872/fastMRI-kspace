7aec5c6c8194c502275d15da256b00324db75534
veritas9872 <veritas9872@gmail.com>
2019-06-04 13:06:53 +0900

    Changed compression function details.

--- main.py
+++ main.py
@@ -21,24 +21,26 @@ from models.unet_model import ResidualUnetModel
 def main():
     defaults = dict(
         batch_size=1,  # This MUST be 1 for now.
-        sample_rate=1,  # Mostly for debugging purposes.
-        num_workers=1,
+        sample_rate=0.01,  # Mostly for debugging purposes.
+        num_workers=2,
         init_lr=1E-3,
         log_dir='./logs',
         ckpt_dir='./checkpoints',
         gpu=1,  # Set to None for CPU mode.
-        num_epochs=20,
-        max_to_keep=2,
+        num_epochs=1,
+        max_to_keep=1,
         verbose=False,
         save_best_only=True,
         data_root='/media/veritas/F/compFastMRI',  # Using compressed dataset for better I/O performance.
         challenge='multicoil',
         center_fractions=[0.08, 0.04],
         accelerations=[4, 8],
-        max_images=4,  # Maximum number of images to save.
+        max_images=0,  # Maximum number of images to save.
         chans=32,
         num_pool_layers=4,
-        converted=True,
+
+        converted=False,
+
         pin_memory=False,
         add_graph=False
     )
