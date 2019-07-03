5ca7655642aae312663fa6dece96769e544aaf73
veritas9872 <veritas9872@gmail.com>
2019-07-03 16:52:16 +0900

    Fixed size shaping bug in channel attention. It works properly now.

--- models/ase_unet.py
+++ models/ase_unet.py
@@ -6,8 +6,8 @@ import torch.nn.functional as F
 class ChannelAttention(nn.Module):
     def __init__(self, num_chans, reduction=16, use_gap=True, use_gmp=True):
         super().__init__()
-        self.gap = nn.AdaptiveAvgPool2d(1)  # Global Average Pooling.
-        self.gmp = nn.AdaptiveMaxPool2d(1)  # Global Maximum Pooling.
+        self.gap = nn.AdaptiveAvgPool2d(output_size=(1, 1))  # Global Average Pooling.
+        self.gmp = nn.AdaptiveMaxPool2d(output_size=(1, 1))  # Global Maximum Pooling.
 
         self.use_gap = use_gap
         self.use_gmp = use_gmp
@@ -21,24 +21,24 @@ class ChannelAttention(nn.Module):
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, tensor):
-
+        batch, chans, _, _ = tensor.shape
         if self.use_gap and self.use_gmp:
-            gap = self.gap(tensor)
-            gmp = self.gmp(tensor)
+            gap = self.gap(tensor).view(batch, chans)
+            gmp = self.gmp(tensor).view(batch, chans)
             # Maybe batch-norm the two pooling types to make their scales more similar.
             # This might make training slower, however.
             features = self.layer(gap) + self.layer(gmp)
-            att = self.sigmoid(features)
+            att = self.sigmoid(features).view(batch, chans, 1, 1)
 
         elif self.use_gap:
-            gap = self.gap(tensor)
+            gap = self.gap(tensor).view(batch, chans)
             features = self.layer(gap)
-            att = self.sigmoid(features)
+            att = self.sigmoid(features).view(batch, chans, 1, 1)
 
         elif self.use_gmp:
-            gmp = self.gmp(tensor)
+            gmp = self.gmp(tensor).view(batch, chans)
             features = self.layer(gmp)
-            att = self.sigmoid(features)
+            att = self.sigmoid(features).view(batch, chans, 1, 1)
 
         else:
             att = 1
@@ -125,7 +125,7 @@ class UnetASE(nn.Module):
 
         self.pool = nn.AvgPool2d(2)
         self.interp = Bilinear()
-        self.input_att = ChannelAttention()
+        self.input_att = ChannelAttention(num_chans=chans, reduction=16, use_gap=True, use_gmp=True)
         self.down_sample_layers = nn.ModuleList()
         ch = chans
 
