cd12fc39f850edc486a9a8d3acd771dc0032e9c3
Hyunjong <hyunjong96@gmail.com>
2019-07-23 14:11:57 +0900

    fully connected extraction layer before Unet

--- data/output_transforms.py
+++ data/output_transforms.py
@@ -135,6 +135,45 @@ class OutputReplaceTransformSemiK(nn.Module):
         return recons  # Returning scaled reconstructions. Not rescaled.
 
 
+class OutputReplaceTransformSemiK2(nn.Module):
+    """
+    Outputs are expected to be k-space data that need reshaping and conversion to the image domain.
+    Inputs and targets are expected to be scaled already.
+    Currently, the implementation expects only 1 batch.
+    Also, I removed rescaling as the loss needs to be calculated on the standardized values for data scale invariance.
+    Final reconstructions can be obtained by dividing by the k_scale value since
+    the Fourier transform and its relatives are all linear functions.
+    """
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, kspace_outputs, targets, extra_params):
+
+        if kspace_outputs.size(0) > 1:
+            raise NotImplementedError('Only one batch at a time for now.')
+
+        # For removing width dimension padding. Recall that k-space form has 2 as last dim size.
+        left = (kspace_outputs.size(-1) - targets['kspace_targets'].size(-2)) // 2
+        right = left + targets['kspace_targets'].size(-2)
+
+        # Cropping width dimension by pad. # Multiply by scales to restore the original scaling. --> Not rescaling!!!
+        kspace_outputs = kspace_outputs[..., left:right]  # * extra_params['k_scales']
+
+        semi_kspace_recons = (nchw_to_kspace(kspace_outputs) + 0.05)
+        # kspace_recons = torch.fft(semi_kspace_recons, signal_ndim=3, normalized=True)
+        kspace_recons = fft1(semi_kspace_recons * extra_params['norm_mask'], direction='height')
+
+        assert kspace_recons.shape == targets['kspace_targets'].shape, 'Reconstruction and target sizes are different.'
+
+        cmg_recons = ifft1(semi_kspace_recons * extra_params['norm_mask'], direction='width')
+
+        img_recons = complex_abs(cmg_recons)
+
+        recons = {'kspace_recons': kspace_recons, 'cmg_recons': cmg_recons, 'img_recons': img_recons, 'semi_kspace_recons': semi_kspace_recons}
+
+        return recons  # Returning scaled reconstructions. Not rescaled.
+
+
 class OutputReplaceTransformLoggedK(nn.Module):
     """
     Outputs are expected to be k-space data that need reshaping and conversion to the image domain.
@@ -200,11 +239,93 @@ class OutputReplaceTransformNormK(nn.Module):
 
         kspace_recons = nchw_to_kspace(kspace_outputs)
 
+        kspace_recons = torch.stack([kspace_recons[..., 0] * torch.cos(kspace_recons[..., 1]), kspace_recons[..., 0] * torch.sin(kspace_recons[..., 1])], dim=-1)
+
         assert kspace_recons.shape == targets['kspace_targets'].shape, 'Reconstruction and target sizes are different.'
 
         cmg_recons = ifft2(kspace_recons * extra_params['norm_mask'])
 
         img_recons = complex_abs(cmg_recons)
+
+        recons = {'kspace_recons': kspace_recons, 'cmg_recons': cmg_recons, 'img_recons': img_recons}
+
+        return recons  # Returning scaled reconstructions. Not rescaled.
+
+
+class OutputReplaceTransformPhaseK(nn.Module):
+    """
+    Outputs are expected to be k-space data that need reshaping and conversion to the image domain.
+    Inputs and targets are expected to be scaled already.
+    Currently, the implementation expects only 1 batch.
+    Also, I removed rescaling as the loss needs to be calculated on the standardized values for data scale invariance.
+    Final reconstructions can be obtained by dividing by the k_scale value since
+    the Fourier transform and its relatives are all linear functions.
+    """
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, kspace_outputs, targets, extra_params):
+
+        if kspace_outputs.size(0) > 1:
+            raise NotImplementedError('Only one batch at a time for now.')
+
+        # For removing width dimension padding. Recall that k-space form has 2 as last dim size.
+        left = (kspace_outputs.size(-1) - targets['kspace_targets'].size(-1)) // 2
+        right = left + targets['kspace_targets'].size(-1)
+
+        # Cropping width dimension by pad. # Multiply by scales to restore the original scaling. --> Not rescaling!!!
+        kspace_outputs = kspace_outputs[..., left:right]  # * extra_params['k_scales']
+
+        # kspace_recons = nchw_to_kspace(kspace_outputs)
+
+        # kspace_recons = torch.stack([extra_params['norm_mask'] * torch.cos(kspace_outputs), extra_params['norm_mask'] * torch.sin(kspace_outputs)], dim=-1)
+        kspace_recons = kspace_outputs
+        assert kspace_recons.shape == targets['kspace_targets'].shape, 'Reconstruction and target sizes are different.'
+
+        cmg_recons = ifft2(torch.stack([extra_params['norm_mask'] * torch.cos(kspace_recons), extra_params['norm_mask'] * torch.sin(kspace_recons)], dim=-1))
+
+        img_recons = complex_abs(cmg_recons)
+
+        recons = {'kspace_recons': kspace_recons, 'cmg_recons': cmg_recons, 'img_recons': img_recons}
+
+        return recons  # Returning scaled reconstructions. Not rescaled.
+
+
+class OutputReplaceTransformCroppedK(nn.Module):
+    """
+    Outputs are expected to be k-space data that need reshaping and conversion to the image domain.
+    Inputs and targets are expected to be scaled already.
+    Currently, the implementation expects only 1 batch.
+    Also, I removed rescaling as the loss needs to be calculated on the standardized values for data scale invariance.
+    Final reconstructions can be obtained by dividing by the k_scale value since
+    the Fourier transform and its relatives are all linear functions.
+    """
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, kspace_outputs, targets, extra_params):
+
+        if kspace_outputs.size(0) > 1:
+            raise NotImplementedError('Only one batch at a time for now.')
+
+        # For removing width dimension padding. Recall that k-space form has 2 as last dim size.
+        left = (kspace_outputs.size(-1) - targets['kspace_targets'].size(-2)) // 2
+        right = left + targets['kspace_targets'].size(-2)
+
+        # Cropping width dimension by pad. # Multiply by scales to restore the original scaling. --> Not rescaling!!!
+        kspace_outputs = kspace_outputs[..., left:right]  # * extra_params['k_scales']
+
+        kspace_recons = nchw_to_kspace(kspace_outputs)
+
+        assert kspace_recons.shape == targets['kspace_targets'].shape, 'Reconstruction and target sizes are different.'
+
+        # kspace_recons = kspace_recons * (1 - extra_params['masks']) + targets['kspace_targets'] * extra_params['masks']
+
+        cmg_recons = ifft1(kspace_recons, direction='width')
+
+        img_recons = complex_abs(cmg_recons)
+
+        kspace_recons = fft1(kspace_recons, direction='height')
 
         recons = {'kspace_recons': kspace_recons, 'cmg_recons': cmg_recons, 'img_recons': img_recons}
 
